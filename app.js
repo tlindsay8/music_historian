bd63dcc70a699eca3e869c81cb3a1051713380da
unknown <giddeon2012@gmail.com>
2020-04-01 21:15:11 -0400

    added index page, closes #5'

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-//let express = express();
-//let app = express();
-//let port = 8080;
+let express = require('express');
+let app = express();
+let port = 8080;
 let request = require("request");
 
-//app.listen(8080);
-let clientID = "d56f39a8e41f4fd18cc3059344f8b825";
-request('https://api.spotify.com/v1/album/Lost', function (error, response, body) {
-  console.error('error: ', error);
-  console.log('statusCode: ', response && response.statusCode);
-  console.log('body: ', body);
-});
+app.use(express.static('/'));
+app.listen(8080);
+// let clientID = "d56f39a8e41f4fd18cc3059344f8b825";
+// request('https://api.spotify.com/v1/album/Lost', function (error, response, body) {
+//   console.error('error: ', error);
+//   console.log('statusCode: ', response && response.statusCode);
+//   console.log('body: ', body);
+// });
 // TODO: Send a GET Request to Spotify api
 /*
 app.get('/', function(req, res){
