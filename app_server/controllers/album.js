8dce7cff40f0b5cac4aa6320a334eb2da902200d
tyler <giddeon2012@gmail.com>
2020-04-18 19:13:45 -0400

    Added data to view. Closes#3

--- app_server/controllers/album.js
+++ app_server/controllers/album.js
@@ -2,50 +2,36 @@ var keys = require('../routes/keys');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotifyKeys);
 
-const albumInfo = (req, res) => {
-  res.render('index', { title: 'Spotify Music Results' });
-};
-
-let albumGetResults = (req, res) => {
+let albumGetResults = (req, res, next) => {
   var type = req.body.type;
 
   console.log(type);
   var plural = type +  's';
   results = [];
-  spotify.search({ type: 'album', query: type, limit: 5 }, function (err, data) {
+  spotify.search({ type: 'track', query: type, limit: 20 }, function (err, data) {
     if (err) {
-      console.log('Error occured' + err);
-      return next(err);
+      console.log('Error occured: ' + err);
+      return err;
     } else {
-
-      data.albums.items.forEach(function (ea) {
+      data.tracks.items.forEach(function (ea) {
           results.push({ artist: ea.artists[0].name,
-                        album: ea.images[2].url,
-                        release_date: ea.release_date,
+                        album: ea.album.images[2].url,
+                        album_name: ea.album.name,
+                        release_date: ea.album.release_date,
                         name: ea.name,
+                        artistPage: ea.artists[0].external_urls.spotify,
 
                       });
-
-          return res
-                .status(200)
-                .render('albums', { results: results });
-
-
+                      
         });
+
+              return res
+                .status(201)
+                .render('albums', { results: results });
     }
   });
-  console.log();
 };
-
-/*
-let returnAlbumInfo = (req, res) => {
-  var results = albumGetResults();
-  console.log(reults);
-
-};
-*/
 
 module.exports = {
-  albumInfo,
   albumGetResults,
 };
